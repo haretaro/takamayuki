62d355c9143bb807104bb1ddd38f75538d9f8550
haretaro <frontend.1000@gmail.com>
2015-08-31 00:39:13 +0900

    update takamayuki.py

--- takamayuki.py
+++ takamayuki.py
@@ -1,4 +1,21 @@
+import os
+import random
+import MeCab
 from twihoge import TwitterApi
+from markov import Markov
 
+os.chdir(os.path.dirname(__file__))
 twitter = TwitterApi.load_json_file('keys.json')
-print(twitter.trendwords())
+
+trend = random.choice(twitter.trendwords())
+statustexts = twitter.search_statustexts(trend,100)
+text = ''
+for status in statustexts:
+    text += status
+
+tagger = MeCab.Tagger('-O wakati')
+sentence = tagger.parse(text).rstrip('\n').split(' ')
+
+Markov(sentence,2)
+
+print(Markov.create())
