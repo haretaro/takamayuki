787b896016ad1c86a0e9b3ccc4233f0ee9752591
haretaro <frontend.1000@gmail.com>
2015-08-31 17:41:03 +0900

    update takamayuki.py markov.py

--- markov.py
+++ markov.py
@@ -2,15 +2,9 @@ import random
 
 class Markov:
     
-    def __init__(self,sentence,depth):
+    def __init__(self,depth):
         self.depth = depth
-        dic = {}
-        for i in range(len(sentence)-depth):
-            sequence = tuple(sentence[i:i+depth])
-            if sequence not in dic:
-                dic[sequence] = []
-            dic[sequence].append(sentence[i+depth])
-        self.dic = dic
+        self.dic = {}
 
     def make_salad(self,limit=100):
         key = random.choice(list(self.dic.keys()))
@@ -25,7 +19,19 @@ class Markov:
             result += word
         return result
 
+    def update(self,sentence):
+        if isinstance(sentence,str):
+            sentence = sentence.rstrip('\n').split(' ')
+        for i in range(len(sentence)-self.depth):
+            sequence = tuple(sentence[i:i+self.depth])
+            if sequence not in self.dic:
+                self.dic[sequence] = []
+            self.dic[sequence].append(sentence[i+self.depth])
+
+
 if __name__ == '__main__':
     ex_sentence = ['それ','が','彼','の','最後','の','言葉','でし','た','。','彼','が','本当に','無神論者','だっ','た','のか','どうか','が','気がかり','です','。',]
-    m = Markov(ex_sentence,1)
-    print(m.create())
+    m = Markov(1)
+    m.update(ex_sentence)
+    m.update(ex_sentence)
+    print(m.make_salad())
--- takamayuki.py
+++ takamayuki.py
@@ -1,21 +1,21 @@
-import os
 import random
-import MeCab
+import exmecab
 from twihoge import TwitterApi
 from markov import Markov
+import os
 
 os.chdir(os.path.dirname(__file__))
 twitter = TwitterApi.load_json_file('keys.json')
 
 trend = random.choice(twitter.trendwords())
-statustexts = twitter.search_statustexts(trend,100)
-text = ''
-for status in statustexts:
-    text += status
+statustexts = twitter.search_statustexts(trend,200)
 
-tagger = MeCab.Tagger('-O wakati')
-sentence = tagger.parse(text).rstrip('\n').split(' ')
+markov = Markov(2)
+for statustext in statustexts:
+    markov.update(exmecab.wakati(statustext))
 
-Markov(sentence,2)
+salad = markov.make_salad(50)
+if len(salad) > 140:
+    salad = salad[:139]
 
-print(Markov.create())
+twitter.tweet(salad)
