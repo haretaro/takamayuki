f3750ea50f5d000b2bd1e3efea0618233aba468f
hareatro <frontend.1000@gmail.com>
2015-09-01 21:08:42 +0900

    use start_words argument

--- takamayuki.py
+++ takamayuki.py
@@ -14,9 +14,12 @@ markov = Markov(2)
 for statustext in statustexts:
     markov.learn(exmecab.wakati(statustext))
 
-salad = markov.make_salad(50)
+a_sentence = exmecab.wakati(random.choice(statustexts)).split(' ')
+start_words = tuple(a_sentence[:2])
+
+salad = markov.make_salad(50,start_words)
 while len(salad) < 20:
-  salad = markov.make_salad(50)
+  salad = markov.make_salad(50,start_words)
 if len(salad) > 140:
     salad = salad[:139]
 
