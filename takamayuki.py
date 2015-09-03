4b946a1fb0e1f83757c1e82c462259d930b1fd2e
haretaro <frontend.1000@gmail.com>
2015-09-03 22:45:32 +0900

    remove 'RT','http...' from tweers

--- takamayuki.py
+++ takamayuki.py
@@ -4,12 +4,16 @@ import exmecab
 from twihoge import TwitterApi
 from markov import Markov
 import os
+import re
 
 os.chdir(os.path.dirname(__file__))
 twitter = TwitterApi.load_json_file('keys.json')
 
 trend = random.choice(twitter.trendwords())
 statustexts = twitter.search_statustexts(trend,300)
+
+statustexts = [re.sub(r'RT.+:','',statustext) for statustext in statustexts]
+statustexts = [re.sub(r'http.+(\n|\Z)','',statustext) for statustext in statustexts]
 
 markov = Markov(2)
 for statustext in statustexts:
@@ -24,4 +28,8 @@ while len(salad) < 20:
 if len(salad) > 140:
     salad = salad[:139]
 
+print(statustexts)
+print(salad)
+print('len = ' + str(len(salad)))
+print('trend = ' + trend)
 twitter.tweet(salad)
